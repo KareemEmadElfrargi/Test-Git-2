89c678c0fa2b9589bb904e2c9ac413732be92372
Kareem Emad <kareememad8561@gmail.com>
2025-02-18 15:26:00 +0200

    change3

--- app/src/main/java/com/example/lab_6/MainActivity.kt
+++ app/src/main/java/com/example/lab_6/MainActivity.kt
@@ -10,7 +10,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        // Enable edge-to-edge display
+        // Enable edge-to-edge display3
 
     }
 }
